9e62306bb0bdde53509d05f76579b893e7258ac5
BryceEvans <evans.bryce@gmail.com>
2019-02-02 12:45:36 -0700

    app set up, need to run tests.

--- baseball/src/Display/display.js
+++ baseball/src/Display/display.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Dashboard from '../Dashboard/dashboard';
 
+
 class Display extends React.Component {
   constructor(props) {
     super(props);
@@ -13,34 +14,59 @@ class Display extends React.Component {
     }
 
     incrementStrike = () => {
-      console.log('Strike pitched!')
-      let strikes = this.state.strikes + 1;
-      this.setState({ strikes });
-      console.log({ strikes });
+      if (this.state.strikes < 2) {
+        let strikes = this.state.strikes;
+        console.log({ strikes });
+        this.setState({ strikes: this.state.strikes + 1 });
+      } else {
+        let strikes = this.state.strikes;
+        console.log({ strikes });
+        this.setState({ strikes: 0, balls: 0, fouls: 0 });
+      }
+      // console.log('Strike pitched!')
+      // let strikes = this.state.strikes + 1;
+      // this.setState({ strikes });
+      // console.log({ strikes });
     }
 
     incrementBall = () => {
-      console.log('Ball pitched!')
-      let balls = this.state.balls + 1;
-      this.setState({ balls });
-      console.log({ balls });
+      // console.log('Ball pitched!')
+      // let balls = this.state.balls + 1;
+      // this.setState({ balls });
+      // console.log({ balls });
+      if (this.state.balls < 3) {
+        let balls = this.state.balls;
+        console.log({ balls });
+        this.setState({ balls: this.state.balls + 1 });
+      } else {
+        let balls = this.state.balls;
+        console.log({ balls });
+        this.setState({ strikes: 0, balls: 0, fouls: 0 });
+      }
     }
 
     incrementFoul = () => {
-      console.log('Foul pitched!')
-      let fouls = this.state.fouls + 1;
-      this.setState({ fouls });
-      console.log({ fouls });
+      if (this.state.strikes < 2) {
+        this.setState({ strikes: this.state.strikes + 1, fouls: this.state.fouls + 1 });
+      } else if (this.state.strikes === 2) {
+          this.setState({ strikes: this.state.strikes, fouls: this.state.fouls + 1 });
+      }
+      // console.log('Foul pitched!')
+      // let fouls = this.state.fouls + 1;
+      // this.setState({ fouls });
+      // console.log({ fouls });
     }
 
     incrementHit = () => {
-      console.log('Strike pitched!')
-      let hits = this.state.hits + 1;
-      this.setState({ hits });
-      console.log({ hits });
+      this.setState({ hits: this.state.hits + 1, strikes: 0, fouls: 0, balls: 0 })
+      // console.log('Strike pitched!')
+      // let hits = this.state.hits + 1;
+      // this.setState({ hits });
+      // console.log({ hits });
     }
 
     render() {
+
       return (
         <div>
           <Dashboard 
